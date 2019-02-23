ecd854d69080ae94ffaff385fdad2241d83061c9
naseemkk <32509960+naseemkk@users.noreply.github.com>
2019-02-24 00:22:22 +0530

    Create patrolling.c

---
+++ patrolling.c
@@ -0,0 +1,340 @@
+// Burn this code to arduino if you want to use the bot in patrolling mode
+#define ir 2
+ 
+#define trigPin1 3
+#define echoPin1 4
+ 
+#define en1 5
+#define en2 6
+ 
+#define trigPin2 7
+#define echoPin2 8
+ 
+#define switch1 9
+#define switch2 10
+ 
+#define in1 14
+#define in2 15
+#define in3 16
+#define in4 17
+ 
+int count;
+float distance;
+int tmpcount = 0;
+ 
+void setup() {
+  // Setup code to run once:
+ 
+  //IR sensor
+  pinMode(ir, INPUT);
+ 
+  //UltraSonic sensor front
+  pinMode(trigPin1, OUTPUT);
+  pinMode(echoPin1, INPUT);
+  
+  //Motor driver enable
+  pinMode(en1, OUTPUT);
+  pinMode(en1, OUTPUT);
+  
+  //UltraSonic sensor side
+  pinMode(trigPin2, OUTPUT);
+  pinMode(echoPin2, INPUT);
+  
+  //Switch1
+  pinMode(switch1, INPUT);
+ 
+  //switch2
+  pinMode(switch2, INPUT);
+ 
+  //Right set of wheels
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
+ 
+  //Left set of wheels
+  pinMode(in3, OUTPUT);
+  pinMode(in4, OUTPUT);
+ 
+  analogWrite(en1, 70);
+  analogWrite(en2, 70);
+ 
+  Serial.begin(9600);
+}
+ 
+void forward();
+void rightTurn();
+void leftTurn();
+void reroute();
+float ultrasonic(int, int);
+ 
+void loop() {
+  // Main code to run repeatedly:
+  if(digitalRead(switch1) == 0) {
+    count = 20 - tmpcount;
+    forward();
+  
+    rightTurn();
+  
+    count = 20 - tmpcount;
+    forward();
+  
+    rightTurn();
+    
+    count = 20 - tmpcount;
+    forward();
+  
+    rightTurn();
+    
+    count = 20 - tmpcount;
+    forward();
+    
+    rightTurn();
+  }
+}
+ 
+void forward() {
+  while(count > 0) {
+    if(digitalRead(switch2) == 0) {
+      digitalWrite(in1, LOW);
+      digitalWrite(in2, LOW);
+  
+      digitalWrite(in3, LOW);
+      digitalWrite(in4, LOW);
+    }
+    distance = ultrasonic(trigPin1, echoPin1);
+    Serial.println(distance);
+    if(distance > 2 && distance  <30 ) {
+      // Rerouting codes
+      reroute();
+    } else {
+      digitalWrite(in1, HIGH);
+      digitalWrite(in2, LOW);
+ 
+      digitalWrite(in3, HIGH);
+      digitalWrite(in4, LOW);
+      
+      // Check the sensor output
+      if(digitalRead(ir) == HIGH) {
+        while(digitalRead(ir) == HIGH) {
+          // To count a division only once
+        }
+        count--;
+      }
+    }
+  } //While loop
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, LOW);
+  
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, LOW);
+}
+ 
+float ultrasonic(int id1,int id2) {
+  float cm, duration;
+  
+  // The sensor is triggered by a HIGH pulse of 10 or more microseconds.
+  // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
+  digitalWrite(id1, LOW);
+  delayMicroseconds(2);
+  digitalWrite(id1, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(id1, LOW);
+ 
+  // Read the signal from the sensor: a HIGH pulse whose
+  // duration is the time (in microseconds) from the sending
+  // of the ping to the reception of its echo off of an object.
+  duration = pulseIn(id2, HIGH);
+ 
+  // convert the time into a distance
+  cm = (duration / 2) * 0.0344;    // As half duration is required to find the distance
+  Serial.println(cm);
+  return cm;
+}
+ 
+void rightTurn() {
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, LOW);
+ 
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, LOW);
+ 
+  analogWrite(en1, 140);
+  analogWrite(en2, 140);
+  delay(1000);
+ 
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+ 
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, HIGH);
+ 
+  delay(355);
+ 
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, LOW);
+ 
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, LOW);
+ 
+  analogWrite(en1, 70);
+  analogWrite(en2, 70);
+}
+ 
+void leftTurn() {
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, LOW);
+ 
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, LOW);
+ 
+  analogWrite(en1, 140);
+  analogWrite(en2, 140);
+  delay(1000);
+ 
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, HIGH);
+ 
+  digitalWrite(in3, HIGH);
+  digitalWrite(in4, LOW);
+ 
+  delay(352);
+ 
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, LOW);
+ 
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, LOW);
+ 
+  analogWrite(en1, 70);
+  analogWrite(en2, 70);
+}
+ 
+void reroute() {
+  tmpcount = 0;
+  int c;
+  
+  rightTurn();
+  
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+ 
+  digitalWrite(in3, HIGH);
+  digitalWrite(in4, LOW);
+ 
+  distance = ultrasonic(trigPin2, echoPin2);
+  Serial.println(distance);
+ 
+  while(distance > 2 && distance <= 40) { //distance calculated from the left ultrasonic sensor
+    if(digitalRead(ir) == HIGH) { //input from ir sensor to measure the number of rotations
+      while(digitalRead(ir) == HIGH) {
+        //To count a division only once
+      }
+      tmpcount++;
+    }
+    distance = ultrasonic(trigPin2, echoPin2);
+    Serial.println(distance);
+  }
+ 
+  c = 6;
+  while(c > 0) {
+    if(digitalRead(ir) == HIGH) { //input from ir sensor to measure the number of rotations
+      while(digitalRead(ir) == HIGH) {
+        //To count a division only once
+      }
+      c--;
+      tmpcount++;
+    }
+  }
+ 
+  //first
+  leftTurn();
+ 
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+ 
+  digitalWrite(in3, HIGH);
+  digitalWrite(in4, LOW);
+ 
+  c = 8;
+  while(c > 0) {
+    if(count < 1) {
+      rightTurn();
+      return;
+    } else {
+      if(digitalRead(ir) == HIGH) { //input from ir sensor to measure the number of rotations
+        while(digitalRead(ir) == HIGH) {
+          //To count a division only once
+        }
+        c--;
+        count--;
+      }
+    }
+  }
+  
+  distance = ultrasonic(trigPin2, echoPin2);
+  Serial.println(distance);
+  
+  while(distance > 2 && distance <= 30) { //distance calculated from the left ultrasonic sensor
+    if(count < 1) {
+      rightTurn();
+      return;
+    } else {
+      if(digitalRead(ir) == HIGH) { //input from ir sensor to measure the number of rotations
+        while(digitalRead(ir) == HIGH) {
+          //To count a division only once
+        }
+        count--;
+      }
+    }
+    distance = ultrasonic(trigPin2, echoPin2);
+    Serial.println(distance);
+  }
+ 
+  c = 6;
+  while(c > 0) {
+    if(count < 1) {
+      rightTurn();
+      return;
+    } else {
+      if(digitalRead(ir) == HIGH) { //input from ir sensor to measure the number of rotations
+        while(digitalRead(ir) == HIGH) {
+          //To count a division only once
+        }
+        c--;
+        count--;
+      }
+    }
+  }
+ 
+  //second
+  leftTurn();
+ 
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+ 
+  digitalWrite(in3, HIGH);
+  digitalWrite(in4, LOW);
+  
+  c = 8;
+  while(c > 0) {
+    if(digitalRead(ir) == HIGH) { //input from ir sensor to measure the number of rotations
+      while(digitalRead(ir) == HIGH) {
+        //To count a division only once
+      }
+      c--;
+      tmpcount--;
+      Serial.println("tmpcount=");
+      Serial.println(tmpcount);
+    }
+  }
+  
+  while(tmpcount > 0) {
+    if(digitalRead(ir) == HIGH) { //input from ir sensor to measure the number of rotations
+      while(digitalRead(ir) == HIGH) {
+        //To count a division only once
+      }
+      tmpcount--;
+    }
+  }
+  
+  rightTurn();
+}
