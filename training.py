1067ad3f3f1a28a5591e86c84adddea657756bfd
naseemkk <32509960+naseemkk@users.noreply.github.com>
2019-02-24 00:23:48 +0530

    Create training.py

---
+++ training.py
@@ -0,0 +1,61 @@
+# Import numpy for matrix calculation
+import numpy as np
+ 
+# Import Python Image Library (PIL)
+from PIL import Image
+ 
+# Create Local Binary Patterns Histograms for face recognization
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+ 
+# Using prebuilt frontal face training model, for face detection
+cascadePath = "haarcascade_frontalface_default.xml"
+detector = cv2.CascadeClassifier(cascadePath)
+ 
+ 
+# Create method to get the images and label data
+def getImagesAndLabels(path):
+    # Get all file path
+    imagePaths = [os.path.join(path, f) for f in os.listdir(path)]
+ 
+    # Initialize empty face sample
+    faceSamples = []
+ 
+    # Initialize empty id
+    ids = []
+ 
+    # Loop all the file path
+    for imagePath in imagePaths:
+ 
+        # Get the image and convert it to grayscale
+        PIL_im = cv2.imread(imagePath)
+        PIL_img=cv2.cvtColor(PIL_im,cv2.COLOR_BGR2GRAY)
+        # PIL image to numpy array
+        img_numpy = np.array(PIL_img, 'uint8')
+ 
+        # Get the image id
+        id = int(os.path.split(imagePath)[-1].split(".")[1])
+        print(id)
+ 
+        # Get the face from the training images
+        faces = detector.detectMultiScale(img_numpy)
+ 
+        # Loop for each face, append to their respective ID
+        for (x, y, w, h) in faces:
+            # Add the image to face samples
+            faceSamples.append(img_numpy[y:y + h, x:x + w])
+ 
+            # Add the ID to IDs
+            ids.append(id)
+ 
+    # Pass the face array and IDs array
+    return faceSamples, ids
+ 
+ 
+# Get the faces and IDs
+faces, ids = getImagesAndLabels('dataset')
+ 
+# Train the model using the faces and IDs
+recognizer.train(faces, np.array(ids))
+ 
+# Save the model into trainer.yml
+recognizer.write('trainer/trainer.yml')
